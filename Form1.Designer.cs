2c4a8dca38aaacda4e7f2a5721f286b30449173e
Aizat Murtazin <aayzat2004@gmail.com>
2023-04-18 18:03:56 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_run = new System.Windows.Forms.Button();
+            this.richTextBox_decryort = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_enc = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_text = new System.Windows.Forms.RichTextBox();
+            this.richTextBox9 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(64, 405);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 16);
+            this.label7.TabIndex = 46;
+            this.label7.Text = "d";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 378);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 16);
+            this.label6.TabIndex = 45;
+            this.label6.Text = "y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 352);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 16);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "e";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 43;
+            this.label4.Text = "phi(N)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 16);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "N";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 16);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "q";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 16);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "p";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 39;
+            this.button2.Text = "decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 38;
+            this.button1.Text = "enc";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button_run
+            // 
+            this.button_run.Location = new System.Drawing.Point(179, 153);
+            this.button_run.Name = "button_run";
+            this.button_run.Size = new System.Drawing.Size(75, 23);
+            this.button_run.TabIndex = 37;
+            this.button_run.Text = "run";
+            this.button_run.UseVisualStyleBackColor = true;
+            this.button_run.Click += new System.EventHandler(this.button_run_Click);
+            // 
+            // richTextBox_decryort
+            // 
+            this.richTextBox_decryort.Location = new System.Drawing.Point(295, 378);
+            this.richTextBox_decryort.Name = "richTextBox_decryort";
+            this.richTextBox_decryort.Size = new System.Drawing.Size(706, 96);
+            this.richTextBox_decryort.TabIndex = 25;
+            this.richTextBox_decryort.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(695, 378);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(316, 96);
+            this.richTextBox5.TabIndex = 34;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox_enc
+            // 
+            this.richTextBox_enc.Location = new System.Drawing.Point(295, 221);
+            this.richTextBox_enc.Name = "richTextBox_enc";
+            this.richTextBox_enc.Size = new System.Drawing.Size(706, 96);
+            this.richTextBox_enc.TabIndex = 33;
+            this.richTextBox_enc.Text = "";
+            // 
+            // richTextBox_text
+            // 
+            this.richTextBox_text.Location = new System.Drawing.Point(295, 80);
+            this.richTextBox_text.Name = "richTextBox_text";
+            this.richTextBox_text.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox_text.Size = new System.Drawing.Size(706, 96);
+            this.richTextBox_text.TabIndex = 32;
+            this.richTextBox_text.Text = "";
+            // 
+            // richTextBox9
+            // 
+            this.richTextBox9.Location = new System.Drawing.Point(114, 405);
+            this.richTextBox9.Name = "richTextBox9";
+            this.richTextBox9.Size = new System.Drawing.Size(140, 24);
+            this.richTextBox9.TabIndex = 31;
+            this.richTextBox9.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(114, 378);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(140, 24);
+            this.richTextBox7.TabIndex = 30;
+            this.richTextBox7.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(114, 356);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(140, 24);
+            this.richTextBox6.TabIndex = 29;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(114, 326);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(140, 24);
+            this.richTextBox4.TabIndex = 28;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(114, 263);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(140, 24);
+            this.richTextBox3.TabIndex = 27;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.Location = new System.Drawing.Point(114, 293);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.Size = new System.Drawing.Size(140, 24);
+            this.richTextBox8.TabIndex = 26;
+            this.richTextBox8.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(114, 233);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(140, 24);
+            this.richTextBox2.TabIndex = 36;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(114, 80);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(140, 47);
+            this.richTextBox1.TabIndex = 35;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 555);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_run);
+            this.Controls.Add(this.richTextBox_decryort);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox_enc);
+            this.Controls.Add(this.richTextBox_text);
+            this.Controls.Add(this.richTextBox9);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_run;
+        private System.Windows.Forms.RichTextBox richTextBox_decryort;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox_enc;
+        private System.Windows.Forms.RichTextBox richTextBox_text;
+        private System.Windows.Forms.RichTextBox richTextBox9;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Threading;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        BigInteger p, q, N, e, d;
+        public static RandomNumberGenerator rng = RandomNumberGenerator.Create();
+        Random randomm = new Random();
+        public static string alf_en = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890.,?!";
+        string nums = "12234567890 ";
+        string text = "";
+
+        int n;
+        public bool string_in(char a, string s) // функция, которая проверяет наличие того или иного элемента в алфавите 
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (a == s[i])
+                    return true;
+            }
+            return false;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private BigInteger rand_bits_len_odd(int n) // ПОЛУЧАЕМ РАНДОМНЫОЕ НЕЧЕТНОЕ ЧИСЛО
+        {
+
+            Random random = new Random();
+            BigInteger rez = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (random.Next(0, 2) == 1)
+                    rez += BigInteger.Pow(2, i);
+            }
+            return rez + 1;
+        }
+        private BigInteger rand_bits_len_odd(Random random, int n) // ПОЛУЧАЕМ РАНДОМНЫОЕ НЕЧЕТНОЕ ЧИСЛО
+        {
+
+
+            BigInteger rez = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (random.Next(0, 2) == 1)
+                    rez += BigInteger.Pow(2, i);
+            }
+            return rez + 1;
+        }
+        private void rand_p() // ПОЛУЧАЕМ РАНДОМНЫОЕ НЕЧЕТНОЕ ЧИСЛО
+        {
+
+
+            BigInteger rez = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (randomm.Next(0, 2) == 1)
+                    rez += BigInteger.Pow(2, i);
+            }
+            rez += 1;
+            p = rez;
+        }
+        private void rand_q() // ПОЛУЧАЕМ РАНДОМНЫОЕ НЕЧЕТНОЕ ЧИСЛО
+        {
+
+            Random randomm = new Random();
+            BigInteger rez = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (randomm.Next(0, 2) == 1)
+                    rez += BigInteger.Pow(2, i);
+            }
+            rez += 1;
+            q = rez;
+        }
+        private static BigInteger ExtendedEuclideanAlgorithm(BigInteger a, BigInteger b, out BigInteger x, out BigInteger y)
+        {
+            if (a == 0)
+            {
+                x = 0;
+                y = 1;
+                return b;
+            }
+
+            BigInteger x1, y1;
+            BigInteger gcd = ExtendedEuclideanAlgorithm(b % a, a, out x1, out y1);
+
+            x = y1 - (b / a) * x1;
+            y = x1;
+
+            return gcd;
+        }
+        public static bool is_Prime_Miller(BigInteger R) // алг Миллера для проверки на простоту
+        {
+            BigInteger t;
+            int s;
+            s_t(R, out t, out s);
+            int k = (int)BigInteger.Log(R, 2.0);
+            for (int i = 0; i < k; i++)
+            {
+                BigInteger b = BigInteger.ModPow(GetRand_Miller(R), t, R);
+                if (b == 1 || b == R - 1)
+                    continue;
+                for (int r = 1; r < s; r++)
+                {
+                    // x ← x^2 mod n
+                    b = BigInteger.ModPow(b, 2, R);
+
+                    // если x == 1, то вернуть "составное"
+                    if (b == 1)
+                        return false;
+
+                    // если x == n − 1, то перейти на следующую итерацию внешнего цикла
+                    if (b == R - 1)
+                        break;
+
+                }
+                if (b != R - 1)
+                    return false;
+            }
+            return true;
+        }
+        public BigInteger ModularExponentiation(BigInteger x, BigInteger y, BigInteger modulus)
+        {
+            if (modulus == 1)
+            {
+                return 0;
+            }
+
+            BigInteger result = 1;
+            x = x % modulus;
+
+            while (y > 0)
+            {
+                if (y % 2 == 1)
+                {
+                    result = (result * x) % modulus;
+                }
+
+                y = y / 2;
+                x = (x * x) % modulus;
+            }
+
+            return result;
+        }
+        public static BigInteger GetRand_Miller(BigInteger R)   // получение рандомного числа для Миллера
+        {
+            byte[] _a = new byte[R.ToByteArray().LongLength];
+            BigInteger b;
+            do
+            {
+                rng.GetBytes(_a);
+                b = new BigInteger(_a);
+            }
+            while (b < 2 || b >= R - 2);
+            return b;
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+        private BigInteger phi(BigInteger n)
+        {
+            BigInteger result = n;
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    while (n % i == 0)
+                    {
+                        n /= i;
+                    }
+                    result -= result / i;
+                }
+            }
+            if (n > 1)
+            {
+                result -= result / n;
+            }
+            return result;
+        }
+        public delegate void ThreadStart();
+
+        private void button_dec_Click(object sender, EventArgs e)
+        {
+            rsa_dec();
+        }
+        private void rsa_dec()
+        {
+            try
+            {
+
+                if (p == 0) throw new Exception("Ключи не сгенtрированы");
+
+                //
+                text = richTextBox_enc.Text;
+                if (text.Length == 0) throw new Exception("Исходный текст пуст");
+                //
+                for (int i = 0; i < text.Length; i++)
+                    if (!string_in(text[i], nums)) throw new Exception("В тексте присутствует лишние символы ( другой язык, пробелы и т.д.)");
+
+                string res = "", help = "";
+                BigInteger r;
+                foreach (char c in text)
+                {
+                    if (c == ' ')
+                    {
+                        if (!BigInteger.TryParse(help, out r))
+                            throw new Exception("Incorrect data");
+                        res += alf_en[(int)(BigInteger.ModPow(r, d, N) - 2)];
+                        help = "";
+                    }
+                    else
+                        help += c;
+                }
+                richTextBox_decryort.Text = res;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Ошибка: {e.Message}");
+
+            }
+        }
+        private void button_en_Click(object sender, EventArgs e)
+        {
+            rsa_encrypt();
+        }
+
+        private void button_run_Click(object sender, EventArgs e)
+        {
+            RSA_en();
+        }
+
+        private void RSA_en()
+        {
+            try
+            {
+                if (richTextBox1.Text.Length == 0) throw new Exception("incorrect num");
+
+                if (!int.TryParse(richTextBox1.Text, out int n)) throw new Exception("incorrect num");
+                if (n > 1024 || n < 2) throw new Exception("out of range 2-1024");
+
+                Thread myThread1 = new Thread(() =>
+                {
+
+                    while (true)
+                    {
+                        p = rand_bits_len_odd(randomm, n);
+                        if (is_Prime_Miller(p))
+                            break;
+                    }
+                });
+                Thread myThread2 = new Thread(() =>
+                {
+
+                    while (true)
+                    {
+                        q = rand_bits_len_odd(randomm, n);
+                        if (is_Prime_Miller(q))
+                            break;
+                    }
+                });
+
+                myThread1.Start();
+                myThread2.Start();
+
+                myThread1.Join();
+                myThread2.Join();
+
+
+
+                N = BigInteger.Multiply(q, p);
+                BigInteger qq = BigInteger.Multiply((p - 1), (q - 1));
+
+
+                while (true)
+                {
+                    e = rand_bits_len_odd((n * 2) / 3);
+                    if (is_Prime_Miller(e) && qq % e != 0)
+                        break;
+                }
+                BigInteger x, y = 0;
+                ExtendedEuclideanAlgorithm(qq, e, out x, out y);
+                d = qq + y;
+                ///
+
+                ///
+                richTextBox2.Text = Convert.ToString(q);
+                richTextBox3.Text = Convert.ToString(p);
+                richTextBox8.Text = Convert.ToString(N);
+                richTextBox4.Text = Convert.ToString(qq);
+                richTextBox6.Text = Convert.ToString(e);
+                richTextBox7.Text = Convert.ToString(y);
+                richTextBox9.Text = Convert.ToString(d);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Ошибка: {e.Message}");
+
+            }
+
+
+
+
+        }
+        private void rsa_encrypt()
+        {
+            try
+            {
+
+                if (p == 0) throw new Exception("Ключи не сгенtрированы");
+
+                //
+                text = richTextBox_text.Text;
+                if (text.Length == 0) throw new Exception("Исходный текст пуст");
+                //
+                for (int i = 0; i < text.Length; i++)
+                    if (!string_in(text[i], alf_en)) throw new Exception("В тексте присутствует лишние символы ( другой язык, пробелы и т.д.)");
+
+                string res = "";
+
+                foreach (char c in text)
+                {
+                    res += BigInteger.ModPow((BigInteger)(alf_en.IndexOf(c) + 2), e, N).ToString() + " ";
+                }
+                richTextBox_enc.Text = res;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Ошибка: {e.Message}");
+
+            }
+        }
+        public static void s_t(BigInteger R, out BigInteger t, out int s)   // получение параметров с и т
+        {
+            t = R - 1;
+            for (s = 0; t % 2 != 1; s++)
+                t = t / 2;
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///
+
+        private void button_run_factorizachiya_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button_run_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private BigInteger fucnk(BigInteger n)
+        {
+            n = n ^ 2 + 1;
+            return n;
+        }
+
+
+    }
+}
